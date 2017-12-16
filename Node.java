3c4b1f69a9147a81cadbf533f2339572b49304d1
Tressos-Aristomenis <34586179+Tressos-Aristomenis@users.noreply.github.com>
2017-12-16 03:18:35 +0200

    Create Node.java

---
+++ Node.java
@@ -0,0 +1,10 @@
+class Node {
+	    public double score;
+	    public PointData ptd;
+	    public Node next;
+	
+	    public Node(double[] x, double y) {
+	    	ptd = new PointData(x, y);							// Ο constructor της Node.
+	    }
+	    
+}
